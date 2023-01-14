6c42d8f6f3407c6ff56e2d7c0617514e66a0fdf7
volodie <119340399+volodie@users.noreply.github.com>
2023-01-14 13:54:26 +0300

    Lab12

---
+++ authorazation/AuthorizationClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace authorazation
+{
+    public class AuthorizationClass
+    {
+        public static (string Login, string Password, string ConfirmPassword) InputLoginAndPassword()
+        {
+            Console.Write("Enter login : ");
+            string login = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+            Console.Write("Enter password confirmation: ");
+            string confirmPassword = Console.ReadLine();
+
+            return (login, password, confirmPassword);
+        }
+
+        public static bool Check(string login, string pass, string confirmPassword)
+        {
+            if (login.Length >= 20 ) { throw new WrongLoginException("Login is too long! Max login length is 20 symbols"); }
+            if (login.Contains(" ")) { throw new WrongLoginException(); }
+            if (pass != confirmPassword) { throw new WrongPasswordException(); }
+
+            bool isDigitPresent = pass.Any(c => char.IsDigit(c));
+
+            if (!isDigitPresent)
+            { 
+                throw new WrongPasswordException("Password doesn't contains any number");
+            }
+            return true;
+        }
+
+        public class WrongLoginException : Exception
+        {
+            public WrongLoginException() : base("You type a wrong login")
+            {
+
+            }
+            public WrongLoginException(string message) : base(message)
+            {
+
+            }
+        }
+
+        public class WrongPasswordException : Exception
+        {
+            public WrongPasswordException() : base("You type a wrong password")
+            {
+
+            }
+            public WrongPasswordException(string message) : base(message)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ authorazation/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using authorazation;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        (string lgn, string psw, string confirmpsw) input = AuthorizationClass.InputLoginAndPassword();
+        string login = input.Item1, password = input.Item2, confirmpassword = input.Item3;
+
+        if (AuthorizationClass.Check(login, password, confirmpassword))
+        {
+            Console.WriteLine("Input login and password is correct!");
+        }
+
+    }
+}
